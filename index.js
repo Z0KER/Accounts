0890a665dfbcb85cbb098e82a5c118293d4504f3
zoker <flaviogabriel2603@gmail.com>
2022-08-16 20:55:43 -0300

    Consulta de saldo

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function operation() {
                 createAccount()
                 break
             case 'Consultar Saldo':
+                getAccountBalance()
                 break
             case 'Depositar':
                 deposit()
@@ -133,7 +134,7 @@ function addAmount(accountName, amount) {
         console.log(err)
     })
 
-    console.log(chalk.green(`Foi depositado o valor de R$ ${amount} na sua conta`))
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta`))
 }
 
 function getAccount(accountName) {
@@ -143,4 +144,26 @@ function getAccount(accountName) {
     })
 
     return JSON.parse(accountJSON)
+}
+
+// show account balance
+function getAccountBalance() {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Qual o nome da sua conta?'
+    }]).then((answer) => {
+        const accountName = answer['accountName']
+
+        // verify if account exists
+        if(!checkAccounts(accountName)) {
+            return getAccountBalance()
+        }
+
+        const accountData = getAccount(accountName)
+
+        console.log(chalk.bgBlue.black(`Olá, o saldo da sua conta é de R$${accountData.balance}`))
+        operation()
+    }).catch((err) => {
+        console.log(err)
+    })
 }
